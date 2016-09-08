082239060c72ba7ecd538686a53272878c17a13b
Eduardo Calandrini <eduardocalandrini@hotmail.com>
2016-09-08 18:07:53 -0300

    base do projeto

---
+++ src/file.c
@@ -0,0 +1,6 @@
+#include <stdlib>
+
+void loadFIle(char* filename){
+  FILE *arq;
+  arq = fopen(filename,"r");
+}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include "file.h"
 
-int int main(int argc, char const *argv[]) {
+typedef struct roteador{
+  int num;
+  FILE *arq;
+  struct roteador* vizinho;
+}Roteador;
+
+typedef struct lista{
+  Roteador* roteador;
+  struct lista* next;
+}Lista;
+
+int main(int argc, char const *argv[]) {
+  /* variables */
+  char no[20];
   /* code */
+  printf("Hello World\n");
   return 0;
+}
+
+void menu(){
+  printf("Inclua o nó e seus vizinhos:\n", );
+  scanf("%s\n", );
 }
